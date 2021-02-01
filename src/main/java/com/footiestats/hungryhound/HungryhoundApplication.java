405fcfb2d6161e2d78b89959a66c798399f44882
Armand Gaillard <armandgaillard@armands-air.home>
2021-02-01 01:34:57 +0000

    added a return

--- src/main/java/com/footiestats/hungryhound/HungryhoundApplication.java
+++ src/main/java/com/footiestats/hungryhound/HungryhoundApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HungryHoundApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(HungryHoundApplication.class, args);
 	}
